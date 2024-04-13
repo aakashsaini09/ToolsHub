81074e057d0798d019d8da6757a237de31e09a1d
aakashsaini09 <aakashsaini948585@gmail.com>
2024-04-13 13:02:54 +0530

    adding run comand

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { MongoClient, ObjectId } from 'mongodb';
 const app = express()
+// app.use(express.static('dist'))
 app.use(cors());
 const uri = "mongodb+srv://aakash:FMTeGe40LV2l1nNP@cluster0.hio303d.mongodb.net/";
 const client = new MongoClient(uri);
