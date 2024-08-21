1964979cac708702ddd08028e37c2a9bc912adb4
aakashsaini09 <aakashsaini948585@gmail.com>
2024-08-21 16:43:22 +0530

    url s

--- src/tools/url_short/index.tsx
+++ src/tools/url_short/index.tsx
@@ -1,5 +1,20 @@
+import axios from "axios"
+import { useState } from "react"
 
 const Url_short = () => {
+    const [userUrl, setuserUrl] = useState('')
+    const [sortedUrl, setsortedUrl] = useState('')
+    const convertUrl = async() => {
+        try {  
+            const res = await axios.post('https://shrtlnk.dev/api/v2/link',{url: userUrl}, 
+                {headers: { 'api-key': 'ZlNBz8KDuWmSLloaAq2WGwogZaNEP6uwn5N49hhzAou8a', 'Accept': 'application/json','Content-Type': 'application/json'
+                  }});
+            const data = await res.data
+            setsortedUrl(data.shrtlnk);
+        } catch (err) {
+            console.log(err)
+        }
+    }
   return (
     <>
       <div className="main h-[100vh] w-[100vw] bg-gray-400 flex justify-center items-center">
@@ -13,8 +28,7 @@ const Url_short = () => {
   </div>
   <div className="flex flex-col gap-4 p-6">
     <div className="relative h-11 w-full min-w-[200px]">
-      <input
-        className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+      <input value={userUrl} onChange={(e)=> setuserUrl(e.target.value)} className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
         placeholder=" " />
       <label
         className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -23,25 +37,16 @@ const Url_short = () => {
     </div>
     <div className="relative h-11 w-full min-w-[200px]">
         {/* Shorted Url */}
-      <input
-        className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-        placeholder=" " />
-
+      <div className="w-full h-full px-3 py-3 flex justify-between font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
+        <span>{sortedUrl} </span><i className="fa-solid fa-copy mr-5 cursor-pointer text-lg"></i></div>
+        
     </div>
   </div>
   <div className="p-6 pt-0">
-    <button
-      className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    <button onClick={convertUrl} className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       type="button">
-      Sign In
+      Convert URL
     </button>
-    {/* <p className="flex justify-center mt-6 font-sans text-sm antialiased font-light leading-normal text-inherit">
-      Don't have an account?
-      <a href="#signup"
-        className="block ml-1 font-sans text-sm antialiased font-bold leading-normal text-blue-gray-900">
-        Sign up
-      </a>
-    </p> */}
   </div>
 </div>
       </div>
