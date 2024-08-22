a92a123b6393494fbdd4b1d84734e7c6b1b47713
aakashsaini09 <aakashsaini948585@gmail.com>
2024-08-22 14:49:39 +0530

    fix responsive

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <img src={toolIcon} className="h-8" alt="Flowbite Logo" />
         <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">ToolsHub</span>
     </Link>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+    <div className="w-full md:block md:w-auto" id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
        
         <li>
