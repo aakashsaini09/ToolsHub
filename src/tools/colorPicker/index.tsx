4d2ed4f4554e641ab380ea2131d29f344b22fb00
aakashsaini09 <aakashsaini948585@gmail.com>
2024-09-18 18:13:14 +0530

    color picker updated

--- src/tools/colorPicker/index.tsx
+++ src/tools/colorPicker/index.tsx
@@ -1,23 +1,35 @@
-import { useState } from 'react';
-import ColorPicker from 'react-pick-color';
-import MiniNav from '../../common/components/MiniNav';
-const ColorPickerComp  = () => {
-    const [color, setColor] = useState('#be00ff');
-  return <>
-  <MiniNav/>
-  <div className='w-[100vw] h-[100vh] bg-[#1e211f] flex flex-col justify-center items-center'>
-    <div className='text-center font-bold text-5xl text-white font-serif mb-7'>Color Picker</div>
-      <ColorPicker color={color} onChange={color => setColor(color.hex)}
-      theme={{
-        background: 'transparent',
-        inputBackground: 'white',
-        borderColor: 'darkgray',
-        borderRadius: '8px',
-        color: 'black',
-        width: '40vw',
-      }}/>;
+import { useEffect, useState } from "react";
+import { FaCopy } from "react-icons/fa";
+// @ts-ignore
+import { SketchPicker } from "react-color";
+import toast from "react-hot-toast";
+
+function App() {
+  const [userColor, setuserColor] = useState("")
+  const [color, setColor] = useState("#3a2fd6"); // Default color
+
+  const handleChangeComplete = (color: any) => {
+    setColor(color.hex); 
+    setuserColor(color.hex)
+  };
+  function copyFunction() {
+    toast.success("Color Copied!!")
+    navigator.clipboard.writeText(userColor); 
+}
+useEffect(() => {
+  console.log(userColor)
+}, [userColor])
+
+  return (
+    <div className="flex justify-center items-center h-auto bg-[#212121] ">
+      <div className="p-6 rounded-lg shadow-lg bg-gray-800">
+        <h1 className="text-2xl font-semibold mb-4 text-slate-300">Color Picker</h1>
+        <SketchPicker color={color}onChangeComplete={handleChangeComplete} className="mb-4 bg-[#212121]" width="33vw" styles={{ default: { picker: { background: "#f2fefb", borderRadius: "0.5rem", setColor: "#3a2fd6" }, }, }}/>
+        <div className="w-full h-16 rounded-lg" style={{ backgroundColor: color }}></div>
+        <p className="mt-4 text-lg text-slate-100 flex">Selected Color: <span className="font-bold text-white">{color}</span><span className="ml-3 cursor-pointer mt-0 hover:text-gray-400" onClick={copyFunction}><FaCopy /></span></p>
+      </div>
     </div>
-    </>
+  );
 }
 
-export default ColorPickerComp
+export default App;
